18f0dfdb497395136fe668c7d86f39e29f366067
rafdc14 <ricardo.afd.costa@gmail.com>
2026-03-19 21:47:42 +0000

    fluxograma feito

---
+++ fluxograma/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace IndoParaTrabalho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        // variavens dentro do (...)  Propriedades
+        {
+            Console.WriteLine("=== SEQUENCIA : INDO PARA O TRABALHO ===\n");
+             // inicio
+             Console.WriteLine("1. INICIO");
+            // sequencia
+            Console.WriteLine("2. ACORDEI");
+            Console.WriteLine("3. VESTI");
+            Console.WriteLine("4. SAI DE CASA");
+            Console.WriteLine("5. ENTREI NO AUTOCARRO");
+            Console.WriteLine("6. ENTREI NO COMBOIO");
+            Console.WriteLine("7. CHEGUEI AO TRABALHO");
+
+            //FIM
+            Console.WriteLine("8. FIM");
+
+
+            Console.WriteLine("=== FIM DA SEQUENCIA ===\n");
+            
+        }// fim metodo Main        
+
+    }// fim de program
+}//fim do namespace
